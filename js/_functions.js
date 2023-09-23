ede9d3da0052633d6762c2f6437bdcf6d2071ff9
AndreyGalena <lilyga12.13@gmail.com>
2023-09-23 18:10:12 +0300

    new-version 23.09.23 18:10

--- js/_functions.js
+++ js/_functions.js
@@ -1,7 +1,1 @@
-import { ctx, cvs } from './file.js';
 
-// Задаём размер игровой доски.
-export function screenSize() {
-    cvs.width = 1920;
-    cvs.height = 930;
-}
--- js/file.js
+++ js/file.js
@@ -1,11 +1,30 @@
-import { screenSize } from "./_functions.js";
+// Название ёмкости.
+let inputName  = document.querySelector('.input-name');
+let namberTank = document.querySelector('.block-namberTank');
 
-export let cvs = document.getElementById("canvas"), // cvs = canvas
-ctx = cvs.getContext('2d');  // cts = context
-            
+// Переменные in/out
+let inputIn   = document.querySelector('.inMeters');
+let out = document.querySelector('.out');
 
-            
-// Задать размер игровой доски.
-screenSize();
-console.log(cvs.width);
-console.log(cvs.height);
+// inputIn.addEventListener('input', () => {
+//     out.innerHTML = (inputIn.value * 27.5);
+// });
+
+inputIn.oninput = function() {
+    out.innerHTML = (inputIn.value * 27.5);
+}
+
+inputName.oninput = function() {
+    // приобразует строку в число с помощю +
+    // даже если type="number"
+    // console.log( inputName.value );
+    if((+inputName.value > 3 && +inputName.value < 8) || inputName == null) {
+        namberTank.innerHTML = `<h3 class="namberTank">${inputName.value}</h3>`;
+        console.log( 'if' );
+    } else {
+        // inputName.value = '4';
+        console.log( inputName.value );
+        console.log( 'else' );
+    }
+    // namberTank.innerHTML = `<h3 class="namberTank">${inputName.value}</h3>`;
+}
