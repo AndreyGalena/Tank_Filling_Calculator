4c439842ce87d1e6503da43e5ab08b166ea3fcf1
AndreyGalena <lilyga12.13@gmail.com>
2023-11-04 15:19:07 +0200

    04.11.23 15:19

--- js/file.js
+++ js/file.js
@@ -1,17 +1,19 @@
 // Поле ввода для названия ёмкости.
-let inputNameOne = document.querySelector('.input-name-1');
-let inputNameTwo = document.querySelector('.input-name-2');
+let inputNameOne = document.querySelector('.input-nameOne');
+let inputNameTwo = document.querySelector('.input-nameTwo');
 
 // Название ёмкости на img-tank
-let namberTankOne = document.querySelector('.block-namberTank-1');
-let namberTankTwo = document.querySelector('.block-namberTank-2');
+let namberTankOne = document.querySelector('.block-namberTankOne');
+let namberTankTwo = document.querySelector('.block-namberTankTwo');
 
-// Переменные in/out
-let inputInOne = document.querySelector('.inMeters-1');
-let outOne     = document.querySelector('.out-1');
+// Переменные in/out/outIn
+let inputInOne = document.querySelector('.inMetersOne');
+let outOne     = document.querySelector('.outOne');
+let outOneIn   = document.querySelector('.outOneIn');
 
-let inputInTwo = document.querySelector('.inMeters-2');
-let outTwo     = document.querySelector('.out-2');
+let inputInTwo = document.querySelector('.inMetersTwo');
+let outTwo     = document.querySelector('.outTwo');
+let outTwoIn   = document.querySelector('.outTwoIn');
 
 // Культура
 let culturaOne = document.querySelector('.culturaOne');
@@ -35,18 +37,18 @@ culturaTwo.onchange = function() {
 inputNameOne.oninput = function() {
     // приобразует строку в число с помощю '+' (даже если type="number")
     if((+inputNameOne.value > 3 && +inputNameOne.value < 8) || inputNameOne == null) {
-        namberTankOne.innerHTML = `<h3 class="namberTank-1">${inputNameOne.value}</h3>`;
+        namberTankOne.innerHTML = `<h3 class="namberTankOne">${inputNameOne.value}</h3>`;
     } else {
-        namberTankOne.innerHTML = '<h3 class="namberTank-1">?</h3>';
+        namberTankOne.innerHTML = '<h3 class="namberTankOne">?</h3>';
     };
 };
 
 inputNameTwo.oninput = function() {
     // приобразует строку в число с помощю '+' (даже если type="number")
     if((+inputNameTwo.value > 13 && +inputNameTwo.value < 20) || inputNameTwo == null) {
-        namberTankTwo.innerHTML = `<h3 class="namberTank-2">${inputNameTwo.value}</h3>`;
+        namberTankTwo.innerHTML = `<h3 class="namberTankTwo">${inputNameTwo.value}</h3>`;
     } else {
-        namberTankTwo.innerHTML = '<h3 class="namberTank-2">?</h3>';
+        namberTankTwo.innerHTML = '<h3 class="namberTankTwo">?</h3>';
     };
 };
 
@@ -54,7 +56,9 @@ inputNameTwo.oninput = function() {
 // Вызывается при изменении значения в поле ввода(вводные данные).
 // Ёмкость №4-7
 inputInOne.oninput = function() {
-    outOne.innerHTML = (inputInOne.value * 27.5);
+    let t = (culturaOne.value * inputInOne.value) / heightOneTanc;
+    outOne.innerHTML = Math.round(t); // округляем.
+    outOneIn.innerHTML = Math.round(culturaOne.value - t);
 };
 // Ёмкость №14-19
 inputInTwo.oninput = function() {
@@ -62,5 +66,6 @@ inputInTwo.oninput = function() {
     // outTwo.innerHTML = (culturaTwo.value * inputInTwo.value) / heightTwoTanc;
     // Осталось до верха заполнить.
     let t = (culturaTwo.value * inputInTwo.value) / heightTwoTanc;
-    outTwo.innerHTML = culturaTwo.value - t;
+    outTwo.innerHTML = Math.round(t);
+    outTwoIn.innerHTML = Math.round(culturaTwo.value - t);
 };
