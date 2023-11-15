c5e576974722beca8623f93b1e14f138d85c354f
AndreyGalena <lilyga12.13@gmail.com>
2023-11-15 18:08:14 +0200

    15.11.23 V-1.07

--- js/file.js
+++ js/file.js
@@ -21,19 +21,26 @@ let culturaTwo = document.querySelector('.culturaTwo');
 
 // Высота ёмкости.
 const heightOneTanc = 32;
-const heightTwoTanc = 19;
+const heightTwoTanc = 21.7;
 
-// Значение культуры.
+// Холостой ход рулетки.
+// Ёмкость 4 - 7.
+const idlingOne = 1.5;
+// Ёмкость 14 - 19.
+const idlingTwo = 2.3;
+
+// Значение культуры ( ТЕХНИЧЕСКАЯ ФУНКЦИЯ для проверки значения(зависит от культуры)).
 culturaOne.onchange = function() {
     console.log(+culturaOne.value); // на выходе int
 };
 
 culturaTwo.onchange = function() {
     console.log(this.value); // на выходе string
-    console.log(Object.values(this));
+    console.log(Object.values(this)); // значения объекта вызвавшее эту функцию.
 }
 
 // Вызывается при изменении значения в поле ввода(номер ёмкости).
+// 4 - 7 ёмкости.
 inputNameOne.oninput = function() {
     // приобразует строку в число с помощю '+' (даже если type="number")
     if((+inputNameOne.value > 3 && +inputNameOne.value < 8) || inputNameOne == null) {
@@ -42,7 +49,7 @@ inputNameOne.oninput = function() {
         namberTankOne.innerHTML = '<h3 class="namberTankOne">?</h3>';
     };
 };
-
+// 14 - 19 ёмкости.
 inputNameTwo.oninput = function() {
     // приобразует строку в число с помощю '+' (даже если type="number")
     if((+inputNameTwo.value > 13 && +inputNameTwo.value < 20) || inputNameTwo == null) {
@@ -56,25 +63,29 @@ inputNameTwo.oninput = function() {
 // Вызывается при изменении значения в поле ввода(вводные данные).
 // Ёмкость №4-7
 inputInOne.oninput = function() {
-    if (inputInOne.value > 32) {
+    if (inputInOne.value > (heightOneTanc + idlingOne)) {
         outOne.innerHTML   = '?';
         outOneIn.innerHTML = '?';
+    }  else if (inputInOne.value < idlingOne) {
+        outOne.innerHTML = '???';
+        outOneIn.innerHTML = '???';
     } else {
-        let t = (culturaOne.value * inputInOne.value) / heightOneTanc;
+        let t = (culturaOne.value * (inputInOne.value - idlingOne)) / heightOneTanc;
         outOne.innerHTML = Math.round(t); // округляем.
         outOneIn.innerHTML = Math.round(culturaOne.value - t);
     };
 };
 // Ёмкость №14-19
 inputInTwo.oninput = function() {
-    // Заполнено
-    // outTwo.innerHTML = (culturaTwo.value * inputInTwo.value) / heightTwoTanc;
     // Осталось до верха заполнить.
-    if (inputInTwo.value > 19) {
+    if (inputInTwo.value > (heightTwoTanc + idlingTwo)) {
         outTwo.innerHTML = '?';
         outTwoIn.innerHTML = '?';
+    } else if (inputInTwo.value < idlingTwo) {
+        outTwo.innerHTML = '???';
+        outTwoIn.innerHTML = '???';
     } else {
-        let t = (culturaTwo.value * inputInTwo.value) / heightTwoTanc;
+        let t = (culturaTwo.value * (inputInTwo.value - idlingTwo)) / heightTwoTanc;
         outTwo.innerHTML = Math.round(t);
         outTwoIn.innerHTML = Math.round(culturaTwo.value - t);
     };
